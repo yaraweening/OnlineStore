5bae9083218f9748d6016ae80ab101f9d37a2a34
Yara Weening <yara.weening@wearetriple.com>
2022-11-23 14:30:09 +0100

    devfile.yml

--- DAL/Interfaces/IProductDAL.cs
+++ DAL/Interfaces/IProductDAL.cs
@@ -6,7 +6,6 @@ namespace DAL.Interfaces
     {
         Product CreateProduct(Product product);
         Product UploadImage(string productID, string image);
-
         string DownloadImage(string productID);
         IEnumerable<Product> GetProducts();
         Product GetProductById(string productID);
